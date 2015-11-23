15369b340a5833af256336ce5c98e95e2f483119
Li Yicong <liyicong_dalian@163.com>
2015-11-23 22:12:25 +0800

    Create Project

---
+++ MoRan/MoRan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MoRan
+//
+//  Created by LYC on 15/11/23.
+//  Copyright © 2015年 GeekBand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MoRan/MoRan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MoRan
+//
+//  Created by LYC on 15/11/23.
+//  Copyright © 2015年 GeekBand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
