ea11a767f7f8a220f494c21e7e66d0d5e97ebf51
Li Yicong <liyicong_dalian@163.com>
2015-12-02 21:40:20 +0800

    loginVC and Logo Image

--- MoRan/MoRan/LoginVC.m
+++ MoRan/MoRan/LoginVC.m
@@ -19,22 +19,35 @@
 
 - (void)loadView {
     [super loadView];
-    /* titleView色值 */
-    CGRect titleViewRect = CGRectMake(0, 0, fullSrceenWidth(), 348);
+
+    CGRect titleViewRect = CGRectMake(0, 0, fullSrceenWidth(), fullScreenHeight()*0.3);
     UIView *titleView = [[UIView alloc] initWithFrame:titleViewRect];
     titleView.backgroundColor = RGB(0xee, 0x7f, 0x41);
     [self.view addSubview:titleView];
     
-    CGRect contentFrame = CGRectMake(0, 348, fullSrceenWidth(), fullScreenHeight()-348);
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"logo"]];
+    imageView.frame = CGRectMake(fullSrceenWidth()/2 - 30, 60, 60, 90);
+    [titleView addSubview:imageView];
+    
+    CGRect contentFrame = CGRectMake(0, CGRectGetHeight(titleView.frame), fullSrceenWidth(), fullScreenHeight()-CGRectGetHeight(titleView.frame));
     UIView *contectView = [[UIView alloc] initWithFrame:contentFrame];
     contectView.backgroundColor = RGB(0xeb, 0xec, 0xec);
     [self.view addSubview:contectView];
+    
+    //NSArray *test = [UIFont familyNames];
+    
+    UILabel *email = [[UILabel alloc] initWithFrame:CGRectMake(20, 20, 60, 20)];
+    email.text = @"邮箱：";
+    email.backgroundColor = contectView.backgroundColor;
+    email.textColor = RGB(0x44, 0x44, 0x44);
+    email.font = [UIFont fontWithName:@"Heiti SC" size:14];
+    [contectView addSubview:email];
     
     /* TODO:设置属性 */
     _accountTF = [[UITextField alloc] initWithFrame:CGRectZero];
     
     /* TEST CODE */
-//    [[netDataManager manager] registerWithName:@"quxianglan" password:@"123456" email:@"lycliyicong@163.com" gid:@"1234567"];
+    
     
 }
 
